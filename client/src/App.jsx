3a6e173c25e36ca75922e6017965176a5f3feb27
Yash Jain <105738560+yashjaincodder@users.noreply.github.com>
2024-02-13 18:18:26 +0530

    make pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
-
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Signin from './pages/Signinn'
+import Signup from './pages/Signup'
+import Profile from './pages/Profile'
 const App = () => {
   return (
-    <div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>}></Route>
+      <Route path='/about' element={<About/>}></Route>
+      <Route path='/sign-in' element={<Signin/>}></Route>
+      <Route path='/sign-up' element={<Signup/>}></Route>
+      <Route path='/profile' element={<Profile/>}></Route>
+    </Routes>
       
-    </div>
+    </BrowserRouter>
   )
 }
 
---
+++ client/src/pages/Signinn.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Signinn = () => {
+  return (
+    <div>
+      signin
+    </div>
+  )
+}
+
+export default Signinn
